96a1b5601e1bcb20451b34ed196342ba0b9fd65c
Brephlas <45274910+Brephlas@users.noreply.github.com>
2023-12-09 00:12:06 +0100

    Introduce dirty solution for speed mode

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # dirty-slow-curtain3
-Dirty solution to have both fast and slow speed available for Switchbot Curtain 3
+*Dirty solution to have both fast and slow speed available for Switchbot Curtain 3*
+
+
+This solution requires you to replace `/homeassistant/deps/lib/python3.11/site-packages/switchbot/devices/curtain.py` with the modified file. If you need the original one back, you can get it from the original [repository](https://github.com/Danielhiversen/pySwitchbot/tree/master). 
+
+I used the file `/config/custom_components/switchbot/.slow` as an binary identification between the modes. You can modify the path in the file `curtain.py` by changing the global variable `FILE_SPEED_IDENTIFIER`.
+
+This is a pretty dirty and hacky solution but it is working fine for me and since I am not into integration development I was not able to develop a better solution quickly.
+
+## Steps to apply this to your installation
+
+1) Replace `/homeassistant/deps/lib/python3.11/site-packages/switchbot/devices/curtain.py` with the modified file from this repo
+2)  Adjust `configuration.yaml` and add the directory where the identifier `.slow` file should be saved to the `allowlist_external_dirs`
+    ```
+    allowlist_external_dirs:
+        - "/config/custom_components/switchbot"
+    ```
+
+3) Create directory `/config/custom_components/switchbot` and file `.slow` inside
+4) Write 0 or 1 into the file
+5) Add shell_command to enable or disable slow mode for the curtains
+    ```
+    switchbot_curtain_enable_slow: 'echo "1" > /config/custom_components/switchbot/.slow'
+    switchbot_curtain_disable_slow: 'echo "0" > /config/custom_components/switchbot/.slow'
+    ```
+6) Reboot Home Assistant
+7) Have fun and use it by calling either one of the services before calling the `cover`-services:
+    ```
+    shell_command.switchbot_curtain_enable_slow
+    shell_command.switchbot_curtain_disable_slow
+    ```
+
+## What does this modification do?
+
+Basically, this modification only checks if `0` or `1` is in the file `.slow` and use this as binary identification to either go slow or fast. The following excerpt shows the file read:
+```py
+with open(FILE_SPEED_IDENTIFIER, "r") as fr:
+    r = int(fr.read().strip())
+    if r == 1:
+        self.slow = 1
+    else:
+        self.slow = 0
+```
+
+If you for example call the `cover.open_cover` service, the check looks like the following:
+```py
+if self.slow:
+    return await self._send_multiple_commands(OPEN_KEYS_SLOW)
+else:
+    return await self._send_multiple_commands(OPEN_KEYS)
+```
---
+++ curtain.py
@@ -0,0 +1,236 @@
+"""Library to handle connection with Switchbot."""
+from __future__ import annotations
+
+import logging
+from typing import Any
+
+from .device import REQ_HEADER, SwitchbotDevice, update_after_operation
+
+# Curtain keys
+CURTAIN_COMMAND = "4501"
+FILE_SPEED_IDENTIFIER = "/config/custom_components/switchbot/.slow"
+OPEN_KEYS = [
+    f"{REQ_HEADER}{CURTAIN_COMMAND}010100",
+    f"{REQ_HEADER}{CURTAIN_COMMAND}05ff00",
+]
+OPEN_KEYS_SLOW = [
+    f"{REQ_HEADER}{CURTAIN_COMMAND}010100",
+    f"{REQ_HEADER}{CURTAIN_COMMAND}050100",
+]
+CLOSE_KEYS = [
+    f"{REQ_HEADER}{CURTAIN_COMMAND}010164",
+    f"{REQ_HEADER}{CURTAIN_COMMAND}05ff64",
+]
+CLOSE_KEYS_SLOW = [
+    f"{REQ_HEADER}{CURTAIN_COMMAND}010164",
+    f"{REQ_HEADER}{CURTAIN_COMMAND}050164",
+]
+POSITION_KEYS = [
+    f"{REQ_HEADER}{CURTAIN_COMMAND}0101",
+    f"{REQ_HEADER}{CURTAIN_COMMAND}05ff",
+]  # +actual_position
+POSITION_KEYS_SLOW = [
+    f"{REQ_HEADER}{CURTAIN_COMMAND}0101",
+    f"{REQ_HEADER}{CURTAIN_COMMAND}0501",
+]  # +actual_position
+STOP_KEYS = [f"{REQ_HEADER}{CURTAIN_COMMAND}0001", f"{REQ_HEADER}{CURTAIN_COMMAND}00ff"]
+
+CURTAIN_EXT_SUM_KEY = f"{REQ_HEADER}460401"
+CURTAIN_EXT_ADV_KEY = f"{REQ_HEADER}460402"
+CURTAIN_EXT_CHAIN_INFO_KEY = f"{REQ_HEADER}468101"
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class SwitchbotCurtain(SwitchbotDevice):
+    """Representation of a Switchbot Curtain."""
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Switchbot Curtain/WoCurtain constructor."""
+
+        # The position of the curtain is saved returned with 0 = open and 100 = closed.
+        # This is independent of the calibration of the curtain bot (Open left to right/
+        # Open right to left/Open from the middle).
+        # The parameter 'reverse_mode' reverse these values,
+        # if 'reverse_mode' = True, position = 0 equals close
+        # and position = 100 equals open. The parameter is default set to True so that
+        # the definition of position is the same as in Home Assistant.
+
+        super().__init__(*args, **kwargs)
+        self._reverse: bool = kwargs.pop("reverse_mode", True)
+        self._settings: dict[str, Any] = {}
+        self.ext_info_sum: dict[str, Any] = {}
+        self.ext_info_adv: dict[str, Any] = {}
+
+    async def _send_multiple_commands(self, keys: list[str]) -> bool:
+        """Send multiple commands to device.
+
+        Since we current have no way to tell which command the device
+        needs we send both.
+        """
+        final_result = False
+        for key in keys:
+            result = await self._send_command(key)
+            final_result |= self._check_command_result(result, 0, {1})
+        return final_result
+
+    def readState(self):
+        with open(FILE_SPEED_IDENTIFIER, "r") as fr:
+            r = int(fr.read().strip())
+            if r == 1:
+                self.slow = 1
+            else:
+                self.slow = 0
+
+    @update_after_operation
+    async def open(self) -> bool:
+        """Send open command."""
+        self.readState()
+        if self.slow:
+            return await self._send_multiple_commands(OPEN_KEYS_SLOW)
+        else:
+            return await self._send_multiple_commands(OPEN_KEYS)
+
+    @update_after_operation
+    async def close(self) -> bool:
+        """Send close command."""
+        self.readState()
+        if self.slow:
+            return await self._send_multiple_commands(CLOSE_KEYS_SLOW)
+        else:
+            return await self._send_multiple_commands(CLOSE_KEYS)
+
+    @update_after_operation
+    async def stop(self) -> bool:
+        """Send stop command to device."""
+        return await self._send_multiple_commands(STOP_KEYS)
+
+    @update_after_operation
+    async def set_position(self, position: int) -> bool:
+        """Send position command (0-100) to device."""
+        position = (100 - position) if self._reverse else position
+        hex_position = "%0.2X" % position
+        self.readState()
+        if self.slow:
+            return await self._send_multiple_commands(
+                [key + hex_position for key in POSITION_KEYS_SLOW]
+            )
+        else:
+            return await self._send_multiple_commands(
+                [key + hex_position for key in POSITION_KEYS]
+            )
+
+    def get_position(self) -> Any:
+        """Return cached position (0-100) of Curtain."""
+        # To get actual position call update() first.
+        return self._get_adv_value("position")
+
+    async def get_basic_info(self) -> dict[str, Any] | None:
+        """Get device basic settings."""
+        if not (_data := await self._get_basic_info()):
+            return None
+
+        _position = max(min(_data[6], 100), 0)
+        return {
+            "battery": _data[1],
+            "firmware": _data[2] / 10.0,
+            "chainLength": _data[3],
+            "openDirection": (
+                "right_to_left" if _data[4] & 0b10000000 == 128 else "left_to_right"
+            ),
+            "touchToOpen": bool(_data[4] & 0b01000000),
+            "light": bool(_data[4] & 0b00100000),
+            "fault": bool(_data[4] & 0b00001000),
+            "solarPanel": bool(_data[5] & 0b00001000),
+            "calibration": bool(_data[5] & 0b00000100),
+            "calibrated": bool(_data[5] & 0b00000100),
+            "inMotion": bool(_data[5] & 0b01000011),
+            "position": (100 - _position) if self._reverse else _position,
+            "timers": _data[7],
+        }
+
+    async def get_extended_info_summary(self) -> dict[str, Any] | None:
+        """Get basic info for all devices in chain."""
+        _data = await self._send_command(key=CURTAIN_EXT_SUM_KEY)
+
+        if not _data:
+            _LOGGER.error("%s: Unsuccessful, no result from device", self.name)
+            return None
+
+        if _data in (b"\x07", b"\x00"):
+            _LOGGER.error("%s: Unsuccessful, please try again", self.name)
+            return None
+
+        self.ext_info_sum["device0"] = {
+            "openDirectionDefault": not bool(_data[1] & 0b10000000),
+            "touchToOpen": bool(_data[1] & 0b01000000),
+            "light": bool(_data[1] & 0b00100000),
+            "openDirection": (
+                "left_to_right" if _data[1] & 0b00010000 == 1 else "right_to_left"
+            ),
+        }
+
+        # if grouped curtain device present.
+        if _data[2] != 0:
+            self.ext_info_sum["device1"] = {
+                "openDirectionDefault": not bool(_data[1] & 0b10000000),
+                "touchToOpen": bool(_data[1] & 0b01000000),
+                "light": bool(_data[1] & 0b00100000),
+                "openDirection": (
+                    "left_to_right" if _data[1] & 0b00010000 else "right_to_left"
+                ),
+            }
+
+        return self.ext_info_sum
+
+    async def get_extended_info_adv(self) -> dict[str, Any] | None:
+        """Get advance page info for device chain."""
+
+        _data = await self._send_command(key=CURTAIN_EXT_ADV_KEY)
+        if not _data:
+            _LOGGER.error("%s: Unsuccessful, no result from device", self.name)
+            return None
+
+        if _data in (b"\x07", b"\x00"):
+            _LOGGER.error("%s: Unsuccessful, please try again", self.name)
+            return None
+
+        _state_of_charge = [
+            "not_charging",
+            "charging_by_adapter",
+            "charging_by_solar",
+            "fully_charged",
+            "solar_not_charging",
+            "charging_error",
+        ]
+
+        self.ext_info_adv["device0"] = {
+            "battery": _data[1],
+            "firmware": _data[2] / 10.0,
+            "stateOfCharge": _state_of_charge[_data[3]],
+        }
+
+        # If grouped curtain device present.
+        if _data[4]:
+            self.ext_info_adv["device1"] = {
+                "battery": _data[4],
+                "firmware": _data[5] / 10.0,
+                "stateOfCharge": _state_of_charge[_data[6]],
+            }
+
+        return self.ext_info_adv
+
+    def get_light_level(self) -> Any:
+        """Return cached light level."""
+        # To get actual light level call update() first.
+        return self._get_adv_value("lightLevel")
+
+    def is_reversed(self) -> bool:
+        """Return True if curtain position is opposite from SB data."""
+        return self._reverse
+
+    def is_calibrated(self) -> Any:
+        """Return True curtain is calibrated."""
+        # To get actual light level call update() first.
+        return self._get_adv_value("calibration")
